fb7bb2d2212247a43c35c2d6e2c71881a44ffea2
Anas Karkoukli <anas.karkoukli@admios-sa.com>
2020-05-27 15:00:06 -0300

    change dimensions

--- src/boundingBox.ts
+++ src/boundingBox.ts
@@ -25,6 +25,7 @@ export default abstract class BoundingBox {
       this.width,
       this.height
     );
+
     return this;
   }
 
--- src/constants.ts
+++ src/constants.ts
@@ -1,8 +1,9 @@
 export const paddleFromEdgeDistance = 40;
 export const dimensions = {
   paddle: { height: 150, width: 30 },
-  ball: { height: 20, width: 20 },
-  court: { height: 800, width: 1000 },
+  ball: { height: 30, width: 30 },
+  court: { height: 600, width: 1000 },
+  border: {width: 5 }
 };
 export const colors = {
   border: "#F8F8FA",
