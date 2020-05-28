cab0f74938a5df9a43aaaeca2ab00671e619dddd
Anas Karkoukli <anas.karkoukli@admios-sa.com>
2020-05-28 17:32:03 -0300

    shadow blur

--- src/message.ts
+++ src/message.ts
@@ -13,7 +13,7 @@ export default class Message {
     const { size, family, style } = fonts.message;
     ctx.font = `${style} ${size}px ${family}`;
     ctx.shadowColor = colors.opponent;
-    ctx.shadowBlur = 5;
+    ctx.shadowBlur = 7;
     ctx.fillStyle = colors.text;
     text.forEach((line, index) => {
       ctx.fillText(
